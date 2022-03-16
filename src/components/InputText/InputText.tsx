9abec3ab5531f8465f7c6e87f1e2529f2123324b
GBelther <belthergabriel@gmail.com>
2022-03-15 22:33:47 -0300

    feature: criação de um input text

---
+++ src/components/InputText/InputText.tsx
@@ -0,0 +1,71 @@
+import { ChangeEvent } from "react";
+import { AiOutlineWarning } from "react-icons/ai";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+
+import "./styles.scss";
+
+type Props = {
+  id?: string;
+  label?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChangeValue?: (value: string) => void;
+  disabled?: boolean;
+  feedback?: {
+    type: "success" | "error" | "info";
+    message: string;
+  };
+};
+
+const InputText = ({
+  label = "",
+  placeholder = "",
+  value,
+  onChange,
+  onChangeValue,
+  disabled = false,
+  feedback,
+  ...rest
+}: Props) => {
+  const handleValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(event);
+    onChangeValue && onChangeValue(event.target.value);
+  };
+
+  return (
+    <div {...rest} className="c-input-text">
+      <label>{label}</label>
+      <div className="c-input-text__input-wrapper">
+        <div className="c-input-text__input-wrapper__icon-wrapper">
+          {feedback?.type === "error" ? (
+            <AiOutlineWarning color="#e60000" />
+          ) : (
+            <AiOutlineCheckCircle color="#49d049" />
+          )}
+        </div>
+        <input
+          type="text"
+          className={`c-input-text__input-wrapper--input ${
+            feedback && `c-input-text__input-wrapper--input-${feedback.type}`
+          }`}
+          placeholder={placeholder}
+          value={value}
+          onChange={handleValueChange}
+          disabled={disabled}
+        />
+      </div>
+      {!!feedback && (
+        <p
+          className={`c-input-text--feedback c-input-text--feedback-${
+            feedback.type ?? "error"
+          }`}
+        >
+          {feedback.message}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default InputText;
